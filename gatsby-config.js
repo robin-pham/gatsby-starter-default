eb6fe221d597cc060fa0bf07986d65a920542bf2
Robin Pham <robin.pham@gmail.com>
2021-04-03 23:48:01 -0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Robin Pham`,
+    description: `A site for people named Robin Pham`,
+    author: `robin-pham`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
